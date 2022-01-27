2e314efd2d5db665c5eef11bbdfaa444b0ec7fdc
swapnil-2000 <swapnil-2000@users.noreply.github.com>
2022-01-27 12:19:23 +0530

    original code

--- math.py
+++ math.py
@@ -1,22 +1,12 @@
 # add implementation
-from decimal import DivisionByZero
-
-
 def add(x,y):
-    return x+y
+    pass
 #subtract implementation
 def subtract(x,y):
-    return x-y
+    pass
 #multiply implementation
 def multiply(x,y):
-    return x*y
+    pass
 #divide implementation
 def divide(x,y):
-<<<<<<< HEAD
-    return x/y
-=======
-    if y==0:
-        return DivisionByZero
-    if y>0:
-        return x/y
->>>>>>> c295533... divide implimentation on bug789
+    pass
